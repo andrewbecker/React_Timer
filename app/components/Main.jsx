073cc066363d335b59f8e4df695a302cb51be9f4
Andy <becker.andrew2@gmail.com>
2016-10-17 13:19:23 -0400

    Initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,14 @@
+'use strict';
+import React from 'react';
+import Nav from 'Nav';
+
+const Main = (props) => {
+  return (
+    <div>
+      <Nav/>
+      {props.children}
+    </div>
+  );
+}
+
+export default Main;
---
+++ app/components/Nav.jsx
@@ -0,0 +1,29 @@
+'use strict';
+
+import React from 'react';
+import {Link, IndexLink} from 'react-router';
+
+const Nav = (props) => {
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <ul className="menu">
+          <li className="menu-text">React Time App</li>
+          <li>
+            <IndexLink to="/" activeClassName="active-link">Timer</IndexLink>
+          </li>
+          <li>
+            <Link to="/" activeClassName="active-link">Countdown</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="top-bar-right">
+        <ul className="menu">
+          <li className="menu-text">Created by <a href="https://github.com/andrewbecker">Andy Becker</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
